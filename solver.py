8c8fdc73faa7d9ce246dd167cf3c56ef17309647
Aniruddh Khanwale <akhanwale@berkeley.edu>
2021-05-02 11:38:23 -0700

    Apparently i changed something?

--- solver.py
+++ solver.py
@@ -7,7 +7,7 @@ import glob
 from collections import Counter
 import os
 import math
-
+from itertools import islice
 import numpy as np
 import random
 import heapq
@@ -168,7 +168,7 @@ def greedy_best_edge_removal_small_medium(OGgraph, num_k, num_c):
         if best_city == -1:
             break
 
-        if curr_path_len_best_city< curr_path_len_best_road:
+        if curr_path_len_best_city < curr_path_len_best_road:
             H = B.copy()
             remove_edge_list.append(best_edge)
             path_length = curr_path_len_best_road
@@ -196,7 +196,7 @@ def greedy_best_edge_removal_small_medium(OGgraph, num_k, num_c):
         if nx.is_connected(H) == False:
             print("oh no")
             break
-    return remove_city_list,remove_edge_list
+    return remove_city_list, remove_edge_list
 
 
 def solve(G):
@@ -226,10 +226,15 @@ def solve(G):
         num_k = 100
         num_c = 5
 
-    return simulated_annealing(G, num_k, num_c)
+    return create_heuristic(G, num_k, num_c)
 
     # big bad graph(s) time aaaa
     # notes: find node that repeats the most time, remove that node, run the whole thing again
+
+
+def k_shortest_paths(G, source, target, k, weight=None):
+    return list(islice(nx.shortest_simple_paths(G, source, target, weight=weight), k))
+
 
 def simulated_annealing(G, num_k, num_c):
 
@@ -255,15 +260,17 @@ def simulated_annealing(G, num_k, num_c):
     P = G.copy()
     # print(nodes_from_edges)
     # common = [i[0] for i in a.most_common()]
-        # common_node = common[0]
-       
+    # common_node = common[0]
+
     # print(common_node)
     while nodes_from_edges:
         common_node = np.argmax(nodes_from_edges)
         # print(nodes_from_edges)
         # print("hey im here!")
         if nx.algorithms.shortest_paths.generic.has_path(P, 0, len(G) - 1) and nx.is_connected(P):
-            shortest_path = nx.algorithms.shortest_paths.weighted.dijkstra_path_length(P, 0, len(G) - 1)
+            shortest_path = nx.algorithms.shortest_paths.weighted.dijkstra_path_length(
+                P, 0, len(G) - 1
+            )
         if (
             common_node
             and common_node != 0
@@ -271,8 +278,12 @@ def simulated_annealing(G, num_k, num_c):
             and P.has_node(common_node)
         ):
             P.remove_node(common_node)
-            if nx.algorithms.shortest_paths.generic.has_path(P, 0, len(G) - 1) and nx.is_connected(P):
-                shortest_path = nx.algorithms.shortest_paths.weighted.dijkstra_path_length(P, 0, len(G) - 1)
+            if nx.algorithms.shortest_paths.generic.has_path(P, 0, len(G) - 1) and nx.is_connected(
+                P
+            ):
+                shortest_path = nx.algorithms.shortest_paths.weighted.dijkstra_path_length(
+                    P, 0, len(G) - 1
+                )
                 # prob = np.exp((short_path1 - shortest_path) / (-T))
                 # if (shortest_path >= short_path1) or (random.random() < prob):
                 if (shortest_path >= short_path1) and num_c > 0:
@@ -289,7 +300,7 @@ def simulated_annealing(G, num_k, num_c):
                         # prob = np.exp((short_path1 - shortest_path) / (-T))
                         # print(prob)
                         # if (short_path2 >= short_path1) or (random.random() < prob):
-                        if (short_path2 >= short_path1):
+                        if short_path2 >= short_path1:
                             H = P.copy()
                             remove_edge_list = edge2
                             short_path1 = short_path2
@@ -315,11 +326,128 @@ def simulated_annealing(G, num_k, num_c):
                                 nodescopy, short_path2, edge2 = greedy_edges(P, num_k, num_c)
                                 remove_edge_list = edge2
         nodes_from_edges.pop(common_node)
-        
-                    
-                
-               
+
     return remove_city_list, remove_edge_list
+
+
+def create_heuristic(Ograph, num_edge, num_city):
+    edges_removed = 0
+    cities_removed = 0
+    remove_edge_list = []
+    remove_city_list = []
+    batch = 10
+    for i in range(0, math.ceil(num_edge / batch)):
+        paths = k_shortest_paths(Ograph, 0, len(G) - 1, batch, weight="weight")
+        city_her = {}
+        edge_her = {}
+        her_cnt = 1
+        for path in paths:
+            dists = []
+            for i in range(len(path) - 1):
+                city_her[path[i]] = city_her.get(path[i], 0) + (1 / her_cnt) / random.uniform(
+                    0.75, 1
+                )
+                edge_her[(path[i], path[i + 1])] = edge_her.get((path[i], path[i + 1]), 0) + (
+                    1 / her_cnt
+                ) / random.uniform(0.75, 1)
+            city_her[path[len(path) - 1]] = city_her.get(path[i], 0) + (
+                1 / her_cnt
+            ) / random.uniform(0.75, 1)
+            her_cnt += 2.5
+        city_her = dict(sorted(city_her.items(), key=lambda item: item[1], reverse=True))
+        edge_her = dict(sorted(edge_her.items(), key=lambda item: item[1], reverse=True))
+
+        J = Ograph.copy()
+        # print(city_her)
+        edge_iter = iter(edge_her)
+        city_iter = iter(city_her)
+
+        # combine heuristics with greedy picking
+        i = 0
+        while i < batch:
+            if edges_removed >= num_edge:
+                break
+            C = J.copy()
+            remove_edge = next(edge_iter, None)
+            if remove_edge == None:
+                break
+            if not C.has_node(remove_edge[1]) or not C.has_node(remove_edge[0]):
+                continue
+            if not C.has_edge(remove_edge[0], remove_edge[1]):
+                continue
+            C.remove_edge(remove_edge[0], remove_edge[1])
+            if nx.is_connected(C):
+                remove_edge_list.append(remove_edge)
+                J = C.copy()
+                edges_removed += 1
+                i += 1
+            else:
+                continue
+        i = 0
+        while i < num_city:
+            if cities_removed >= num_city:
+                # print(cities_removed)
+                # print(num_city)
+                break
+            C = J.copy()
+            remove_city = next(city_iter, None)
+            if remove_city == 0 or remove_city == len(G) - 1:
+                continue
+            if remove_city == None:
+                break
+            # print(remove_city)
+            C.remove_node(remove_city)
+            if nx.is_connected(C):
+                remove_city_list.append(remove_city)
+                J = C.copy()
+                cities_removed += 1
+                i += 1
+            else:
+                continue
+        Ograph = J
+
+    return remove_city_list, remove_edge_list
+
+
+def greedy_all_shortest_edges(G, num_k, num_c):
+    all_paths = list(nx.all_shortest_paths(G, 0, len(G) - 1))
+    edge_removed_graph = G.copy()
+    edges_removed = []
+    for i in range(num_k):  # can iterate over up to k edges
+        longest_path = all_paths[0]  # new shortest path
+        longest_path_index = 0
+        optimal_edge = None
+        bridging_edges = list(nx.bridges(edge_removed_graph))
+        for j in range(len(all_paths[0]) - 1):  # iterate over all nodes
+            u = all_paths[0][j]
+            v = all_paths[0][j + 1]  # (u, v)
+            path_ind = 0
+            if (u, v) not in bridging_edges:  # removal won't disconnect graph
+                for long_path in all_paths[1:]:  # check longer paths
+                    path_ind += 1
+                    if u not in long_path and v not in long_path:
+                        if path_ind > longest_path_index:  # new shortest_path
+                            longest_path_index = path_ind
+                            longest_path = long_path
+                            optimal_edge = (u, v)
+                        break
+        if optimal_edge is None:
+            break
+        cp = edge_removed_graph.copy()
+        cp.remove_edge(optimal_edge[0], optimal_edge[1])
+        assert nx.is_connected(cp)
+        edge_removed_graph.remove_edge(optimal_edge[0], optimal_edge[1])
+        edges_removed.append(optimal_edge)
+        new_all_paths = []
+        for p in all_paths:
+            if optimal_edge[0] not in p or (
+                p.index(optimal_edge[0]) + 1 != len(p) - 1
+                and p[p.index(optimal_edge[0]) + 1] != optimal_edge[1]
+            ):
+                new_all_paths.append(p)
+        all_paths = new_all_paths
+    G = edge_removed_graph
+    return [], edges_removed
 
 
 def min_cut_life(G, num_k, num_c):
